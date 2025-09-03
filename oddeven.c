7852de64c15f87162acdb03f4736eaaa58d68a6e
Subhasmita swain <khushiswain712@gmail.com>
2025-09-03 20:11:27 +0530

    oddeven.c
    program to accept two no. from user and display square if even and display cube if odd

---
+++ oddeven.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main ()
+{
+   int a,b, sqr1, sqr2, cubel, cube2;
+   printf("Enter two numbers: ");
+   scanf ("&d %d", &a, &b);
+   if (a%2==0)
+   {sqr1=aжа;
+   printf("The squares of &d is %d\n",a,sqr1 );}
+   else
+   {cube1=ажа*а;
+   printf ("The cubes of sd is sd\n", a, cubel );}
+   if (b%2==0 )
+   {sqr2=b*b:
+   printf ("The squares of &d is sd\n",b, sqr2 );}
+   else
+   {cube2=b*b*b;
+   printf("The cubes of sd is sd\n" ,b, cube2 );}
+   return 0;
+}
